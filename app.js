9ba3fdddc0caa50f1e0344e164fd248033092256
TurqaySafarov <161557826+TurqaySafarov@users.noreply.github.com>
2024-03-27 14:29:48 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,86 @@
+//Daxil olunan ədədin sadə yoxsa mürəkkəb olduğunu tapın.
+
+// let number = +prompt("Ədəd daxil edin: ");
+// for (let i = 2; i < number; i++) {
+//   if (number % i == 0) {
+//     result = true;
+//     break;
+//   } else {
+//     result = false;
+//   }
+// }
+
+// if (result == true) {
+//   console.log("Murekkeb");
+// } else {
+//   console.log("sade");
+// }
+
+
+// Daxil olunan ədədin mükəmməl olub olmadığını tapın.
+
+// let number = +prompt("Ədəd daxil edin: ");
+// let num = 0;
+// for (let i = 1; i < number; i++) {
+//   if (( number % i == 0)) {
+//     sum = num + i;
+//   }
+// }
+// if (number == num) {
+//   console.log("Mükemmel ededdir");
+// } else {
+//   console.log("Mükemmel eded deyil");
+// }
+
+
+// Daxil olunan 2 ədəd arasındakı mükəmməl ədədləri göstərin.
+
+// let number1 = +prompt("birinci ededi daxil edin: ");
+// let number2 = +prompt("Ikinci ededi daxil edin: ");
+
+// for (let i = number1; i <= number2; i++) {
+//   let num = 0;
+//   for (let j = 1; j < i; j++) {
+//     if (i % j == 0) {
+//       num = num + j
+//     }
+//   }
+//   if (num == i) {
+//     console.log(
+//       `${number1} ve ${number2}  arasindaki mukemmel eded: ${i}`
+//     );
+//   }
+// }
+
+
+// Daxil olunan 2 ədəd arasındakı sadə ədədləri göstərin.
+
+// let number1 = +prompt("birinci ededi daxil edin: ");
+// let number2 = +prompt("Ikinci ededi daxil edin: ");
+// console.log(`${num1} ve ${num2} arasindaki sade ededler: `);
+// for (i = number1 + 1; i < number2; i++) {
+//   for (let j = 2; j < i; j++) {
+//     if (i % j == 0) {
+//       result = true;
+//       break;
+//     } else {
+//       result = false;
+//     }
+//   }
+
+//   if (result == false) {
+//     console.log(` ${i}`);
+//   }
+// }
+
+
+// Daxil olunan 2 ədəd arasındakı sonu 7 ilə bitən ədədləri göstərin.
+
+// let number1 = +prompt("Birinci ededi daxil edin: ");
+// let number2 = +prompt("Ikinci ededi daxil edin: ");
+
+// for(let i=number1; i<number2; i++){
+//    if(i%10==7){
+//     console.log(i)
+//    }
+// }
